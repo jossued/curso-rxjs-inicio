88b89b66e4eee6bb008269296144109dc8cf563a
Jossué Dután <jossue.dutan@kushkipagos.com>
2021-05-11 10:39:50 -0500

    un obs de strings

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,20 @@
 const nombre = 'Jossué';
 console.log(nombre);
 
+import {Observable} from 'rxjs';
 
+// const obs$ = Observable.create();
+const obs$ = new Observable<string>(
+    subs => {
+        subs.next('Hola');
+        subs.complete(); // manda que no se va a emitir nada
+        subs.next('Hola 2');
+    }
+);
 
-
+obs$.subscribe(
+    console.log
+)
 
 
 
