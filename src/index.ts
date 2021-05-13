7add2a1ce35724db6314bda322a1bb0148a6dd24
Jossué Dután <jossue.dutan@kushkipagos.com>
2021-05-13 16:46:15 -0500

    ajax

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
-import { range, Observer, fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observer, of } from "rxjs";
+import { ajax, AjaxError } from "rxjs/ajax";
+import { catchError, map } from "rxjs/operators";
 
+const url = 'https://api.github.com/users1?per_page=5';
 const observer: Observer<any> = {
-    next: valor => console.log(valor),
-    error: error => console.error(error),
+    next: valor => console.log({valor}),
+    error: error => console.error({error}),
     complete: () => console.info('completado')
 }
 
-const subs$ = 
-    range(1,5)
-    .pipe(
-        // map recibe y devuelve un valor
-        map<number,string>( // tipar el map
-            (valor) => {
-                return (valor*10).toString();
-            }
-        )
-    );
-
-subs$.subscribe(observer);
-
-const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
-
-keyup$.subscribe(val => console.log(val.code));
+ajax(url)
+.pipe(
+    map(r => r.response),
+    catchError(
+        (err: AjaxError) => {
+            console.warn('error en:', err.message);
+            return of({});
+        }
+    )
+)
+.subscribe(observer);
