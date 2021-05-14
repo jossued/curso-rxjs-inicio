f9b747ac699df27fe7dbb77e2aeea578c12a4731
Jossué Dután <jossue.dutan@kushkipagos.com>
2021-05-14 10:33:05 -0500

    operadores aplanamiento

---
+++ src/transformacion/07-lab-aplanamiento.ts
@@ -0,0 +1,53 @@
+import { fromEvent, of } from 'rxjs';
+import { tap, map, mergeMap, pluck, catchError, switchMap, exhaustMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
+// Helper
+const peticionHttpLogin = ( userPass ) => 
+    ajax.post('https://reqres.in/api/login?delay=1', userPass)
+        .pipe(
+            pluck('response', 'token'),
+            catchError( err => of('xxx') )
+        )
+
+
+
+// creando un formulario
+const form = document.createElement('form');
+const inputEmail = document.createElement('input');
+const inputPass  = document.createElement('input');
+const submitBtn  = document.createElement('button');
+
+// Configuraciones
+inputEmail.type = 'email';
+inputEmail.placeholder = 'Email';
+inputEmail.value = 'eve.holt@reqres.in';
+
+inputPass.type = 'password';
+inputPass.placeholder = 'Password';
+inputPass.value = 'cityslicka';
+
+submitBtn.innerHTML = 'Ingresar';
+
+form.append( inputEmail, inputPass, submitBtn );
+document.querySelector('body').append( form );
+
+// Streams
+const submitForm$ = fromEvent<Event>( form, 'submit' )
+    .pipe(
+        tap( ev => ev.preventDefault() ),
+        map( ev => ({
+            email: ev.target[0].value,
+            password: ev.target[1].value
+        })),
+        exhaustMap( peticionHttpLogin )
+        // mergeMap se subscribe a todos los observables
+        // switchMap cancela el anterior y envia solo el ultimo observable
+        // concatMap espera a que cada observable se complete y lanza el siguiente
+        // exhaustMap lanza la primera emision e ignora las sucesivas (sirve para cual el usuario hace muchos click o peticiones)
+    );
+
+
+submitForm$.subscribe( token => {
+    console.log(token);
+})
